d71181e8ab07af59b125f2eed8f58ba0c832c531
moddyz <moddy3d@gmail.com>
2020-06-30 18:50:40 +1200

    Upgrade upgrade script.

--- upgradeToLatestTemplate.sh
+++ upgradeToLatestTemplate.sh
@@ -3,9 +3,9 @@
 # upgradeToLatestTemplate.sh
 #
 # Utility to upgrade the current CMakeTemplate-based project by pulling in the
-# cmake modules from the the CMakeTemplate repository.
+# cmake modules from the latest state of the CMakeTemplate repository.
 #
-# This tool should be run at the _root_ of the current repository.
+# This tool should be run at the _root_ of the current, local repository.
 
 set -euxo pipefail
 
@@ -13,3 +13,4 @@ set -euxo pipefail
 rm -rf /tmp/CMakeTemplate
 git clone https://github.com/moddyz/CMakeTemplate /tmp/CMakeTemplate
 cp -r /tmp/CMakeTemplate/cmake/macros/* ./cmake/macros/
+cp -r /tmp/CMakeTemplate/upgradeToLatest.sh ./
