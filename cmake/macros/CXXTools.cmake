e67dae2ff8e455ce654bc63490639cb3fa5f54ad
moddyz <moddy3d@gmail.com>
2020-10-11 11:17:46 +1300

    Refactored out BaseTools.cmake

--- cmake/macros/CXXTools.cmake
+++ cmake/macros/CXXTools.cmake
@@ -2,31 +2,6 @@
 # Functions & macros for building C++ libraries and programs.
 #
 
-# List all the sub-directories, under PARENT_DIRECTORY, and store into SUBDIRS.
-macro(
-    list_subdirectories
-    SUBDIRS
-    PARENT_DIRECTORY
-)
-    # Glob all files under current directory.
-    file(
-        GLOB
-        CHILDREN
-        RELATIVE ${PARENT_DIRECTORY}
-        ${PARENT_DIRECTORY}/*
-    )
-
-    set(DIRECTORY_LIST "")
-
-    foreach(CHILD ${CHILDREN})
-        if(IS_DIRECTORY ${PARENT_DIRECTORY}/${CHILD})
-            list( APPEND DIRECTORY_LIST ${CHILD})
-        endif()
-    endforeach()
-
-    set(${SUBDIRS} ${DIRECTORY_LIST})
-endmacro()
-
 # Builds a new C++ library.
 #
 # Single value Arguments:
@@ -266,26 +241,6 @@ function(
         DESTINATION ${CMAKE_INSTALL_PREFIX}
     )
 endfunction()
-
-# Convenience macro for calling add_subdirectory on all the sub-directories
-# in the current source directory.
-macro(
-    add_subdirectories
-)
-    list_subdirectories(
-        SUBDIRS
-        ${CMAKE_CURRENT_SOURCE_DIR}
-    )
-
-    foreach(
-        subdir
-        ${SUBDIRS}
-    )
-        add_subdirectory(
-            ${subdir}
-        )
-    endforeach()
-endmacro()
 
 # Convenience macro for adding the current source directory as a header only library.
 #
