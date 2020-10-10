2d70a74d4c50a7c6042bc325b9254f44d21ac547
moddyz <moddy3d@gmail.com>
2020-10-10 23:08:58 +1300

    Formatted cpp code.

--- src/exampleProgram/main.cpp
+++ src/exampleProgram/main.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-    int lhs = 1;
-    int rhs = 2;
+    int lhs    = 1;
+    int rhs    = 2;
     int result = Add( lhs, rhs );
     printf( "%d + %d = %d\n", lhs, rhs, result );
     return 0;
