a759c87c383b67c5f4b7917ea773a23c13865ad7
moddyz <moddy3d@gmail.com>
2020-10-14 22:23:41 +1300

    Export add symbol

--- src/exampleSharedLibrary/add.h
+++ src/exampleSharedLibrary/add.h
@@ -4,5 +4,12 @@
 ///
 /// Addition operator.
 
+#if defined( _WIN32 ) || defined( _WIN64 )
+#define CXXTEMPLATE_EXPORT __declspec( dllexport )
+#else
+#define CXXTEMPLATE_EXPORT
+#endif
+
 /// Adds integers \p i_lhs and \p i_rhs and returns the result.
+CXXTEMPLATE_EXPORT
 int Add( int i_lhs, int i_rhs );
