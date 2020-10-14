b15ba19d3d2750c76eb13fcffbc5fb143d631bd1
Richard Lei <moddy3d@gmail.com>
2020-10-14 22:52:37 +1300

    Disable doxygen build for windows. (#7)
    * Disable doxygen build for windows.
    
    * Export add symbol
    
    * CMake verbose
    
    * --verbose
    
    * DLL import
    
    * API

--- src/exampleSharedLibrary/add.h
+++ src/exampleSharedLibrary/add.h
@@ -4,5 +4,15 @@
 ///
 /// Addition operator.
 
+#if defined( _WIN32 ) || defined( _WIN64 )
+#if defined( exampleSharedLibrary_EXPORTS )
+#define CXXTEMPLATE_API __declspec( dllexport )
+#else
+#define CXXTEMPLATE_API __declspec( dllimport )
+#endif
+#else
+#define CXXTEMPLATE_API
+#endif
+
 /// Adds integers \p i_lhs and \p i_rhs and returns the result.
-int Add( int i_lhs, int i_rhs );
+CXXTEMPLATE_API int Add( int i_lhs, int i_rhs );
