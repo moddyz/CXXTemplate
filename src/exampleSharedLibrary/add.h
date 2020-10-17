f301e03e2e70be5dd16ad06830642bff03ce81a8
Richard Lei <moddy3d@gmail.com>
2020-10-18 11:02:56 +1300

    Separate storage-class api definitions into api.h (#11)
    * Separate storage-class api definitions into api.h
    
    * Specify api.h in CMakeLists.txt

--- src/exampleSharedLibrary/add.h
+++ src/exampleSharedLibrary/add.h
@@ -4,15 +4,7 @@
 ///
 /// Addition operator.
 
-#if defined( _WIN32 ) || defined( _WIN64 )
-#if defined( exampleSharedLibrary_EXPORTS )
-#define CXXTEMPLATE_API __declspec( dllexport )
-#else
-#define CXXTEMPLATE_API __declspec( dllimport )
-#endif
-#else
-#define CXXTEMPLATE_API
-#endif
+#include <exampleSharedLibrary/api.h>
 
 /// Adds integers \p i_lhs and \p i_rhs and returns the result.
-CXXTEMPLATE_API int Add( int i_lhs, int i_rhs );
+EXAMPLESHAREDLIBRARY_API int Add( int i_lhs, int i_rhs );
